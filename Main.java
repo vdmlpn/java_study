149dbedd5b5c487751cf00ba8421416916ec8139
Vadim Lyapunov <vdmlpn93@gmail.com>
2022-05-16 10:28:45 +0300

    first java example

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main{
+    public static void main(String []args) {
+        String str1 = "The weather is fine.";
+        char[] arrStr = {'s','u','n','n','y'};
+        String str2 = new String (arrStr);
+        System.out.println("Old string: " +str1);
+        String str1_changed = str1.replace("fine", str2.toUpperCase());
+        System.out.println("Changed string will be: " +str1_changed);
+        System.out.println("The length of the new string is " +str1_changed.length());
+    }
+}
